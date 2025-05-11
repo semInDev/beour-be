dec4f27c7d2739ebf13d8f96bb1ad404db6e2292
sem_in_github <semin6634@naver.com>
2025-05-11 15:23:47 +0900

    initial commit

---
+++ src/main/java/com/beour/BeourApplication.java
@@ -0,0 +1,13 @@
+package com.beour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beour/BeourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
