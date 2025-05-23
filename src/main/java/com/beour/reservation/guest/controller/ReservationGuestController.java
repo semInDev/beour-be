38ec627b038d6c9dd9de544ef2f42ae63963eb04
99hyeon <hyeon99@ajou.ac.kr>
2025-05-23 18:47:43 +0900

    [#13] ㄹfeat: guest의 예약 현황 조회 기능 구현

--- src/main/java/com/beour/reservation/guest/controller/ReservationGuestController.java
+++ src/main/java/com/beour/reservation/guest/controller/ReservationGuestController.java
@@ -3,14 +3,19 @@ package com.beour.reservation.guest.controller;
 import com.beour.global.response.ApiResponse;
 import com.beour.reservation.guest.dto.CheckAvailableTimesRequestDto;
 import com.beour.reservation.guest.dto.ReservationCreateRequest;
+import com.beour.reservation.guest.dto.ReservationListResponseDto;
 import com.beour.reservation.guest.dto.ReservationResponseDto;
 import com.beour.reservation.guest.dto.SpaceAvailableTimeResponseDto;
 import com.beour.reservation.guest.service.CheckAvailableTimeService;
 import com.beour.reservation.guest.service.ReservationGuestService;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -28,6 +33,11 @@ public class ReservationGuestController {
     @PostMapping("/api/spaces/reserve/available-times")
     public ApiResponse<SpaceAvailableTimeResponseDto> checkAvailableTimes(@Valid @RequestBody CheckAvailableTimesRequestDto requestDto){
         return ApiResponse.ok(checkAvailableTimeService.findAvailableTime(requestDto));
+    }
+
+    @GetMapping("/api/reservation")
+    public ApiResponse<List<ReservationListResponseDto>> checkReservationList(@RequestParam(value = "guestId") Long guestId){
+        return ApiResponse.ok(reservationGuestService.findReservationList(guestId));
     }
 
 }
--- src/main/java/com/beour/reservation/guest/service/ReservationGuestService.java
+++ src/main/java/com/beour/reservation/guest/service/ReservationGuestService.java
@@ -4,13 +4,19 @@ import com.beour.global.exception.exceptionType.SpaceNotFoundException;
 import com.beour.global.exception.exceptionType.UserNotFoundException;
 import com.beour.reservation.commons.entity.Reservation;
 import com.beour.reservation.commons.enums.ReservationStatus;
+import com.beour.reservation.commons.exceptionType.ReservationNotFound;
 import com.beour.reservation.commons.repository.ReservationRepository;
 import com.beour.reservation.guest.dto.ReservationCreateRequest;
+import com.beour.reservation.guest.dto.ReservationListResponseDto;
 import com.beour.reservation.guest.dto.ReservationResponseDto;
 import com.beour.space.host.entity.Space;
 import com.beour.space.host.repository.SpaceRepository;
 import com.beour.user.entity.User;
 import com.beour.user.repository.UserRepository;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -49,6 +55,22 @@ public class ReservationGuestService {
         return userRepository.findById(userId).orElseThrow(
             () -> new UserNotFoundException("존재하지 않는 유저입니다.")
         );
+    }
+
+    public List<ReservationListResponseDto> findReservationList(Long guestId){
+        List<Reservation> reservationList = reservationRepository.findUpcomingReservationsByGuest(guestId, LocalDate.now(), LocalTime.now());
+
+        if(reservationList.isEmpty()){
+            throw new ReservationNotFound("예약이 없습니다.");
+        }
+
+        List<ReservationListResponseDto> responseDtoList = new ArrayList<>();
+        for (Reservation reservation : reservationList) {
+            responseDtoList.add(ReservationListResponseDto.of(reservation));
+        }
+
+
+        return responseDtoList;
     }
 
 }
