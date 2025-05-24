db6f9b3c58447ec43bdee83fa480e8aba1b69cb7
99hyeon <hyeon99@ajou.ac.kr>
2025-05-24 14:59:23 +0900

    refactor: soft delete 위사용 위해 softdeletableentity 클래스 추가

--- src/main/java/com/beour/user/entity/User.java
+++ src/main/java/com/beour/user/entity/User.java
@@ -2,11 +2,8 @@ package com.beour.user.entity;
 
 import com.beour.global.entity.BaseTimeEntity;
 import com.beour.user.dto.SignupDto;
-import com.beour.user.enums.UserRole;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,61 +16,61 @@ import lombok.NoArgsConstructor;
 @Entity
 public class User extends BaseTimeEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-  @Column(nullable = false, length = 10)
-  private String name;
+    @Column(nullable = false, length = 10)
+    private String name;
 
-  @Column(nullable = false, unique = true, length = 10)
-  private String nickname;
+    @Column(nullable = false, unique = true, length = 10)
+    private String nickname;
 
-  @Column(nullable = false, length = 50)
-  private String email;
+    @Column(nullable = false, length = 50)
+    private String email;
 
-  @Column(unique = true, length = 15)
-  private String loginId;
+    @Column(nullable = false, unique = true, length = 15)
+    private String loginId;
 
-  @Column(nullable = false)
-  private String password;
+    @Column(nullable = false)
+    private String password;
 
-  @Column(nullable = false)
-  private String phone;
+    @Column(nullable = false)
+    private String phone;
 
-  private String role;
+    @Column(nullable = false)
+    private String role;
 
+    @Builder
+    public User(String name, String nickname, String email, String loginId, String password,
+        String phone, String role) {
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+        this.loginId = loginId;
+        this.password = password;
+        this.phone = phone;
+        this.role = role;
+    }
 
-  @Builder
-  public User(String name, String nickname, String email, String loginId, String password,
-      String phone, String role) {
-    this.name = name;
-    this.nickname = nickname;
-    this.email = email;
-    this.loginId = loginId;
-    this.password = password;
-    this.phone = phone;
-    this.role = role;
-  }
+    public static User createFrom(SignupDto signUpDto) {
+        return User.builder()
+            .name(signUpDto.getName())
+            .nickname(signUpDto.getNickname())
+            .email(signUpDto.getEmail())
+            .loginId(signUpDto.getLoginId())
+            .password(signUpDto.getPassword())
+            .phone(signUpDto.getPhone())
+            .role(signUpDto.getRole())
+            .build();
+    }
 
-  public static User createFrom(SignupDto signUpDto) {
-    return User.builder()
-        .name(signUpDto.getName())
-        .nickname(signUpDto.getNickname())
-        .email(signUpDto.getEmail())
-        .loginId(signUpDto.getLoginId())
-        .password(signUpDto.getPassword())
-        .phone(signUpDto.getPhone())
-        .role(signUpDto.getRole())
-        .build();
-  }
-
-  public static User fromJwt(String loginId, String password, String role) {
-    return User.builder()
-        .loginId(loginId)
-        .password(password)
-        .role(role)
-        .build();
-  }
+    public static User fromJwt(String loginId, String password, String role) {
+        return User.builder()
+            .loginId(loginId)
+            .password(password)
+            .role(role)
+            .build();
+    }
 
 }
