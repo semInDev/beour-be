31717fad01557ceffd554f3d7480e454f7a086e7
99hyeon <hyeon99@ajou.ac.kr>
2025-06-16 14:36:55 +0900

    comment: 주석 추가

--- src/main/java/com/beour/user/dto/ChangePasswordRequestDto.java
+++ src/main/java/com/beour/user/dto/ChangePasswordRequestDto.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ChangePasswordRequestDto {
 
+    //todo : 비밀번호 유효성 검증 어노테이션 추가
     @NotBlank(message = "새로운 비밀번호를 입력해주세요.")
     private String newPassword;
 
