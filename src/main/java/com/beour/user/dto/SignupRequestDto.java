a7cf7594f9bdb70622cd37343f71fadcea74e106
99hyeon <hyeon99@ajou.ac.kr>
2025-06-23 14:18:56 +0900

    fix: 회원가입 비밀번호 암호화 버그 수정

--- src/main/java/com/beour/user/dto/SignupRequestDto.java
+++ src/main/java/com/beour/user/dto/SignupRequestDto.java
@@ -6,9 +6,7 @@ import com.beour.global.validator.annotation.ValidPassword;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class SignupRequestDto {
@@ -36,4 +34,8 @@ public class SignupRequestDto {
     @NotBlank(message = "핸드폰번호는 필수입니다.")
     @Pattern(regexp = "^\\d{10,11}$", message = "전화번호는 숫자만 10~11자리로 입력하세요.")
     private String phone;
+
+    public void encodingPassword(String encodedPassword){
+        this.password = encodedPassword;
+    }
 }
