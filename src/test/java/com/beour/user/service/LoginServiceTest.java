553c7aa85473ae9f7858db2c5ecbbfa6b328f63d
99hyeon <hyeon99@ajou.ac.kr>
2025-06-25 14:00:46 +0900

    [#52] test: LoginService의 resetPassword 테스트 코드 작성
    - 해당 유저 존재하지 않을 경우
    - 비밀번호 재발급 성공
    위 두가지 케이스 테스트 코드 작성

--- src/test/java/com/beour/user/service/LoginServiceTest.java
+++ src/test/java/com/beour/user/service/LoginServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.beour.global.exception.exceptionType.UserNotFoundException;
 import com.beour.user.dto.FindLoginIdRequestDto;
 import com.beour.user.dto.FindLoginIdResponseDto;
+import com.beour.user.dto.ResetPasswordRequestDto;
+import com.beour.user.dto.ResetPasswordResponseDto;
 import com.beour.user.entity.User;
 import com.beour.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +14,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
@@ -22,6 +25,8 @@ class LoginServiceTest {
     private UserRepository userRepository;
     @Autowired
     private LoginService loginService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @BeforeEach
     void setUp() {
@@ -56,7 +61,7 @@ class LoginServiceTest {
         //given
         FindLoginIdRequestDto requestDto = FindLoginIdRequestDto.builder()
             .name("유저2")
-            .email("user2@gamil.com")
+            .email("user2@gmail.com")
             .phone("01012345678")
             .build();
 
@@ -72,7 +77,7 @@ class LoginServiceTest {
         //given
         FindLoginIdRequestDto requestDto = FindLoginIdRequestDto.builder()
             .name("탈퇴유저")
-            .email("delete1@gamil.com")
+            .email("delete1@gmail.com")
             .phone("01012345678")
             .build();
 
@@ -99,8 +104,42 @@ class LoginServiceTest {
         assertEquals("user1", result.getLoginId());
     }
 
+    @Test
+    @DisplayName("비밀번호 리셋-일치하는 회원 없음")
+    void resetPassword_user_not_found() {
+        //given
+        ResetPasswordRequestDto requestDto = ResetPasswordRequestDto.builder()
+            .loginId("user1")
+            .name("유저2")
+            .email("user2@gamil.com")
+            .phone("01012345678")
+            .build();
 
+        //when    then
+        UserNotFoundException exception = assertThrows(UserNotFoundException.class,
+            () -> loginService.resetPassword(requestDto));
+        assertEquals("일치하는 회원을 찾을 수 없습니다.", exception.getMessage());
+    }
 
+    @Test
+    @DisplayName("비밀번호 리셋-성공")
+    void success_resetPassword() {
+        //given
+        ResetPasswordRequestDto requestDto = ResetPasswordRequestDto.builder()
+            .loginId("user1")
+            .name("유저1")
+            .email("user1@gmail.com")
+            .phone("01012345678")
+            .build();
 
+        //when
+        User before = userRepository.findByLoginId(requestDto.getLoginId()).orElse(null);
+        ResetPasswordResponseDto result = loginService.resetPassword(requestDto);
+
+        //then
+        User after = userRepository.findByLoginId(requestDto.getLoginId()).orElse(null);
+        assertNotEquals(before.getPassword(), after.getPassword());
+        assertTrue(passwordEncoder.matches(result.getTempPassword(), after.getPassword()));
+    }
 
 }
