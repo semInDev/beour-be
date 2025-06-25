1234c36961ff6918799c12183c27e5e0b27741f6
99hyeon <hyeon99@ajou.ac.kr>
2025-06-25 16:10:18 +0900

    [#52] test: LoginController resetPassword @Valid 테스트 코드 작성

--- src/test/java/com/beour/user/controller/LoginControllerValidationTest.java
+++ src/test/java/com/beour/user/controller/LoginControllerValidationTest.java
@@ -23,15 +23,6 @@ public class LoginControllerValidationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    /**
-     *
-     * 이름
-     * 이메일 공백, 형식
-     * 핸드폰 특수문자
-     * 핸드폰 공백
-     *
-     */
-
     @Test
     @DisplayName("이름 공란")
     void findLogId_invalid_name_blank() throws Exception {
@@ -118,5 +109,115 @@ public class LoginControllerValidationTest {
             .andExpect(jsonPath("$.message").value("전화번호는 숫자만 10~11자리로 입력하세요."));
     }
 
+    @Test
+    @DisplayName("로그인 아이디 공란")
+    void resetPassword_invalid_loginId_blank() throws Exception {
+        //given
+        String requestJson = """
+            {
+                "loginId": "",
+                "name": "유저1",
+                "email": "test@gmail.com",
+                "phone": "01012345678"
+            }
+        """;
+
+        //when then
+        mockMvc.perform(post("/api/users/reset-pw")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson)
+            )
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("아이디는 필수입니다."));
+    }
+
+    @Test
+    @DisplayName("이름 공란")
+    void resetPassword_invalid_name_blank() throws Exception {
+        //given
+        String requestJson = """
+            {
+                "loginId": "user1",
+                "name": "",
+                "email": "test@gmail.com",
+                "phone": "01012345678"
+            }
+        """;
+
+        //when then
+        mockMvc.perform(post("/api/users/reset-pw")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson)
+            )
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("이름은 필수입니다."));
+    }
+
+    @Test
+    @DisplayName("이메일 공란")
+    void resetPassword_invalid_email_blank() throws Exception {
+        //given
+        String requestJson = """
+            {
+                "loginId": "user1",
+                "name": "유저1",
+                "email": "",
+                "phone": "01012345678"
+            }
+        """;
+
+        //when then
+        mockMvc.perform(post("/api/users/reset-pw")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson)
+            )
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("이메일은 필수입니다."));
+
+    }
+
+    @Test
+    @DisplayName("이메일 형식 안맞음")
+    void resetPassword_invalid_email() throws Exception {
+        //given
+        String requestJson = """
+            {
+                "loginId": "user1",
+                "name": "유저1",
+                "email": "gmail.com",
+                "phone": "01012345678"
+            }
+        """;
+
+        //when then
+        mockMvc.perform(post("/api/users/reset-pw")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson)
+            )
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("올바른 이메일 형식이어야 합니다."));
+    }
+
+    @Test
+    @DisplayName("핸드폰 번호 입력 특수문자 포함")
+    void resetPassword_invalid_phone() throws Exception {
+        //given
+        String requestJson = """
+            {
+                "loginId": "user1",
+                "name": "유저1",
+                "email": "",
+                "phone": "0101234567="
+            }
+        """;
+
+        //when then
+        mockMvc.perform(post("/api/users/reset-pw")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson)
+            )
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("전화번호는 숫자만 10~11자리로 입력하세요."));
+    }
 
 }
