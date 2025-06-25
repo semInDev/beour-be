01b5851b8fc27c16f9dd97053a939798af9ffd7c
99hyeon <hyeon99@ajou.ac.kr>
2025-06-25 13:08:51 +0900

    [#6] refactor: test코드 작성 위해 @Builder 추가

--- src/test/java/com/beour/user/service/LoginServiceTest.java
+++ src/test/java/com/beour/user/service/LoginServiceTest.java
@@ -2,9 +2,15 @@ package com.beour.user.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.beour.global.exception.exceptionType.UserNotFoundException;
+import com.beour.global.response.ApiResponse;
+import com.beour.user.dto.FindLoginIdRequestDto;
+import com.beour.user.dto.FindLoginIdResponseDto;
 import com.beour.user.entity.User;
 import com.beour.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -43,8 +49,19 @@ class LoginServiceTest {
             .build();
         deleteUser.softDelete();
         userRepository.save(deleteUser);
-
     }
+
+//    @Test
+//    @DisplayName("로그인아이디찾기-일치하는 회원 없음")
+//    void findLoginId_user_not_found(){
+//        //given
+//        FindLoginIdRequestDto requestDto = new FindLoginIdRequestDto();
+//
+//        //when    then
+//        UserNotFoundException exception = assertThrows(UserNotFoundException.class, () -> loginService.findLoginId(requestDto));
+//        assertEquals("일치하는 회원을 찾을 수 없습니다.", exception.getMessage());
+//    }
+
 
 
 }
