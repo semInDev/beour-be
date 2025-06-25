bc0a73516fd15ce10bdb6528e059d5a77f801964
99hyeon <hyeon99@ajou.ac.kr>
2025-06-25 13:28:32 +0900

    refactor: ResetPasswordRequestDto test 코드 작성 위해 @Builder 추가

--- src/main/java/com/beour/user/dto/ResetPasswordRequestDto.java
+++ src/main/java/com/beour/user/dto/ResetPasswordRequestDto.java
@@ -3,8 +3,8 @@ package com.beour.user.dto;
 import com.beour.global.validator.annotation.ValidPhoneNum;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class ResetPasswordRequestDto {
@@ -21,5 +21,13 @@ public class ResetPasswordRequestDto {
   @NotBlank(message = "이메일은 필수입니다.")
   @Email(message = "올바른 이메일 형식이어야 합니다.")
   private String email;
+
+  @Builder
+  public ResetPasswordRequestDto(String loginId, String name, String phone, String email){
+    this.loginId = loginId;
+    this.name = name;
+    this.phone = phone;
+    this.email = email;
+  }
 
 }
