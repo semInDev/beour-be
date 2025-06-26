0aae7d943c47e19707586fb874ab8b8628420d98
99hyeon <hyeon99@ajou.ac.kr>
2025-06-26 13:11:35 +0900

    comment: LoginControllerTest 작성할 케이스 작성

--- src/test/java/com/beour/user/controller/LoginControllerTest.java
+++ src/test/java/com/beour/user/controller/LoginControllerTest.java
@@ -4,5 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LoginControllerTest {
 
+    /**
+     * 아이디 찾기 - 회원 없을 경우
+     * 아이디 찾기 - 탈퇴한 회원일 경우
+     * 아이디 찾기 - 성공
+     *
+     * 비번 재발급 - 회원 없을 경우
+     * 비번 재발급 - 탈퇴한 회원일 경우
+     * 비번 재발급 - 성공
+     */
+
+
+
 
 }
