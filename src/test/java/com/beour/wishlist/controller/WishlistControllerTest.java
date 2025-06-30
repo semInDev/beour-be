73c51ecab305ef809e49edacad24a98580ba91cb
99hyeon <hyeon99@ajou.ac.kr>
2025-06-30 15:20:41 +0900

    [#68] test: WishlistController addSpaceToWishlist 테스트 코드 작성

---
+++ src/test/java/com/beour/wishlist/controller/WishlistControllerTest.java
@@ -0,0 +1,189 @@
+package com.beour.wishlist.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.beour.global.jwt.JWTUtil;
+import com.beour.space.domain.entity.Space;
+import com.beour.space.domain.entity.Tag;
+import com.beour.space.domain.repository.SpaceRepository;
+import com.beour.space.domain.repository.TagRepository;
+import com.beour.space.host.enums.SpaceCategory;
+import com.beour.space.host.enums.UseCategory;
+import com.beour.user.entity.User;
+import com.beour.user.repository.UserRepository;
+import com.beour.wishlist.entity.Like;
+import com.beour.wishlist.repository.LikeRepository;
+import com.beour.wishlist.service.WishlistService;
+import java.util.ArrayList;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class WishlistControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private SpaceRepository spaceRepository;
+    @Autowired
+    private LikeRepository likeRepository;
+    @Autowired
+    private TagRepository tagRepository;
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    private User guest;
+    private User host;
+    private Space space1;
+    private Space space2;
+    private String accessToken;
+
+
+    @BeforeEach
+    void setUp() {
+        guest = User.builder()
+            .loginId("guest")
+            .password(passwordEncoder.encode("guestpassword!"))
+            .name("게스트")
+            .nickname("guest")
+            .email("guest@gmail.com")
+            .phone("01012345678")
+            .role("GUEST")
+            .build();
+        userRepository.save(guest);
+
+        host = User.builder()
+            .loginId("host1")
+            .password(passwordEncoder.encode("host1password!"))
+            .name("호스트1")
+            .nickname("host1")
+            .email("host1@gmail.com")
+            .phone("01012345678")
+            .role("HOST")
+            .build();
+        userRepository.save(host);
+
+        space1 = Space.builder()
+            .host(host)
+            .name("공간1")
+            .spaceCategory(SpaceCategory.COOKING)
+            .useCategory(UseCategory.COOKING)
+            .maxCapacity(3)
+            .address("서울시 강남구")
+            .detailAddress("투썸건물 2층")
+            .pricePerHour(15000)
+            .thumbnailUrl("https://example.img")
+            .latitude(123.12)
+            .longitude(123.12)
+            .avgRating(0.0)
+            .tags(new ArrayList<>())
+            .build();
+        spaceRepository.save(space1);
+        Tag tag1 = Tag.builder()
+            .space(space1)
+            .contents("쿠킹1")
+            .build();
+        tagRepository.save(tag1);
+        space1.getTags().add(tag1);
+
+        space2 = Space.builder()
+            .host(host)
+            .name("공간2")
+            .spaceCategory(SpaceCategory.COOKING)
+            .useCategory(UseCategory.COOKING)
+            .maxCapacity(3)
+            .address("서울시 강남구")
+            .detailAddress("투썸건물 2층")
+            .pricePerHour(15000)
+            .thumbnailUrl("https://example.img")
+            .latitude(123.12)
+            .longitude(123.12)
+            .avgRating(0.0)
+            .tags(new ArrayList<>())
+            .build();
+        spaceRepository.save(space2);
+        Tag tag2 = Tag.builder()
+            .space(space2)
+            .contents("쿠킹2")
+            .build();
+        tagRepository.save(tag2);
+        space2.getTags().add(tag2);
+
+        accessToken = jwtUtil.createJwt(
+            "access",
+            guest.getLoginId(),
+            "ROLE_" + guest.getRole(),
+            1000L * 60 * 30    // 30분
+        );
+    }
+
+    @AfterEach
+    void tearDown() {
+        tagRepository.deleteAll();
+        likeRepository.deleteAll();
+        spaceRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    /**
+     * 찜하기
+     * - 찜 등록
+     * - 이미 존재하는 공간일 경우
+     *
+     * 찜 삭제
+     * - 없는 공간일 경우
+     * - 성공
+     *
+     * 찜 목록 조회
+     * - 성공
+     * - 아무것도 없을 경우
+     */
+
+    @Test
+    @DisplayName("찜 등록 - 이미 목록에 존재하는 공간")
+    void add_wishlist_with_exist_space() throws Exception {
+        //given
+        Like like = Like.builder()
+            .user(guest)
+            .space(space1)
+            .build();
+        likeRepository.save(like);
+
+        //when  then
+        mockMvc.perform(get("/api/wishlist")
+                .param("spaceId", space1.getId().toString())
+                .header("Authorization", "Bearer " + accessToken)
+            )
+            .andExpect(status().isConflict())
+            .andExpect(jsonPath("$.message").value("wishlist에 존재하는 공간입니다."));
+    }
+
+    @Test
+    @DisplayName("찜 등록 - 성공")
+    void success_add_wishlist() throws Exception {
+        //when  then
+        mockMvc.perform(get("/api/wishlist")
+                .param("spaceId", space1.getId().toString())
+                .header("Authorization", "Bearer " + accessToken)
+            )
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.message").value("찜 등록이 완료되었습니다."));
+    }
+
+}
