019778852536d8e6cca4e6c978c107d61c6be92e
99hyeon <hyeon99@ajou.ac.kr>
2025-07-03 15:35:10 +0900

    [#73] test: ReservationRepository findBySpaceIdAndDateAndDeletedAtIsNull 테스트 코드 추가

---
+++ src/test/java/com/beour/reservation/commons/repository/ReservationRepositoryTest.java
@@ -0,0 +1,182 @@
+package com.beour.reservation.commons.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.beour.reservation.commons.entity.Reservation;
+import com.beour.reservation.commons.enums.ReservationStatus;
+import com.beour.reservation.commons.enums.UsagePurpose;
+import com.beour.reservation.guest.service.ReservationGuestService;
+import com.beour.space.domain.entity.AvailableTime;
+import com.beour.space.domain.entity.Space;
+import com.beour.space.domain.repository.AvailableTimeRepository;
+import com.beour.space.domain.repository.SpaceRepository;
+import com.beour.space.host.enums.SpaceCategory;
+import com.beour.space.host.enums.UseCategory;
+import com.beour.user.entity.User;
+import com.beour.user.repository.UserRepository;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ReservationRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private SpaceRepository spaceRepository;
+    @Autowired
+    private AvailableTimeRepository availableTimeRepository;
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    private User guest;
+    private User host;
+    private Space space;
+    private AvailableTime availableTimePast;
+    private AvailableTime availableTimeCurrent;
+    private AvailableTime availableTimeNext;
+
+
+    @BeforeEach
+    void setUp() {
+        guest = User.builder()
+            .loginId("guest")
+            .password(passwordEncoder.encode("guestpassword!"))
+            .name("게스트")
+            .nickname("guest")
+            .email("guest@gmail.com")
+            .phone("01012345678")
+            .role("GUEST")
+            .build();
+        userRepository.save(guest);
+
+        host = User.builder()
+            .loginId("host1")
+            .password(passwordEncoder.encode("host1password!"))
+            .name("호스트1")
+            .nickname("host1")
+            .email("host1@gmail.com")
+            .phone("01012345678")
+            .role("HOST")
+            .build();
+        userRepository.save(host);
+
+        space = Space.builder()
+            .host(host)
+            .name("공간1")
+            .spaceCategory(SpaceCategory.COOKING)
+            .useCategory(UseCategory.COOKING)
+            .maxCapacity(3)
+            .address("서울시 강남구")
+            .detailAddress("투썸건물 2층")
+            .pricePerHour(15000)
+            .thumbnailUrl("https://example.img")
+            .latitude(123.12)
+            .longitude(123.12)
+            .avgRating(0.0)
+            .availableTimes(new ArrayList<>())
+            .build();
+        spaceRepository.save(space);
+
+        availableTimePast = AvailableTime.builder()
+            .space(space)
+            .date(LocalDate.now().minusDays(1))
+            .startTime(LocalTime.of(1, 0, 0))
+            .endTime(LocalTime.of(23, 0, 0))
+            .build();
+        availableTimeRepository.save(availableTimePast);
+        space.getAvailableTimes().add(availableTimePast);
+
+        availableTimeCurrent = AvailableTime.builder()
+            .space(space)
+            .date(LocalDate.now())
+            .startTime(LocalTime.of(1, 0, 0))
+            .endTime(LocalTime.of(23, 0, 0))
+            .build();
+        availableTimeRepository.save(availableTimeCurrent);
+        space.getAvailableTimes().add(availableTimeCurrent);
+
+        availableTimeNext = AvailableTime.builder()
+            .space(space)
+            .date(LocalDate.now().plusDays(1))
+            .startTime(LocalTime.of(1, 0, 0))
+            .endTime(LocalTime.of(23, 0, 0))
+            .build();
+        availableTimeRepository.save(availableTimeNext);
+        space.getAvailableTimes().add(availableTimeNext);
+    }
+
+    @AfterEach
+    void tearDown() {
+        SecurityContextHolder.clearContext();
+        availableTimeRepository.deleteAll();
+        reservationRepository.deleteAll();
+        spaceRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    @Test
+    @Transactional
+    void findBySpaceIdAndDateAndDeletedAtIsNull_test() {
+        //given
+        Reservation reservation = Reservation.builder()
+            .guest(guest)
+            .host(host)
+            .space(space)
+            .status(ReservationStatus.ACCEPTED)
+            .usagePurpose(UsagePurpose.BARISTA_TRAINING)
+            .requestMessage("테슽뚜")
+            .date(LocalDate.now().plusDays(1))
+            .startTime(LocalTime.of(12, 0, 0))
+            .endTime(LocalTime.of(16, 0, 0))
+            .price(60000)
+            .guestCount(2)
+            .build();
+        reservationRepository.save(reservation);
+
+        Reservation reservation2 = Reservation.builder()
+            .guest(guest)
+            .host(host)
+            .space(space)
+            .status(ReservationStatus.ACCEPTED)
+            .usagePurpose(UsagePurpose.BARISTA_TRAINING)
+            .requestMessage("테슽뚜")
+            .date(LocalDate.now().plusDays(1))
+            .startTime(LocalTime.of(16, 0, 0))
+            .endTime(LocalTime.of(17, 0, 0))
+            .price(15000)
+            .guestCount(2)
+            .build();
+        reservationRepository.save(reservation2);
+        reservation2.softDelete();
+
+        //when
+        List<Reservation> result = reservationRepository.findBySpaceIdAndDateAndDeletedAtIsNull(
+            space.getId(), LocalDate.now().plusDays(1));
+
+        //then
+        assertEquals(1, result.size());
+        assertEquals(reservation.getSpace(), result.get(0).getSpace());
+        assertEquals(reservation.getStartTime(), result.get(0).getStartTime());
+        assertEquals(reservation.getEndTime(), result.get(0).getEndTime());
+    }
+
+
+}
