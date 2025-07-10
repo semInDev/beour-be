4250a1171a31e332d55885479ed3fd54ba49b181
99hyeon <hyeon99@ajou.ac.kr>
2025-07-10 18:41:15 +0900

    [#116] refactor: 대댓글 관련 예외처리 enum 생성

---
+++ src/main/java/com/beour/global/exception/error/errorcode/CommentErrorCode.java
@@ -0,0 +1,15 @@
+package com.beour.global.exception.error.errorcode;
+
+import com.beour.global.exception.error.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommentErrorCode implements ErrorCode {
+
+    COMMENT_ALREADY_EXISTS(409, "이미 해당 리뷰에 대한 댓글이 작성되었습니다.");
+
+    private final Integer code;
+    private final String message;
+}
