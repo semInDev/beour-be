190ddc5a662f5a2d5658a513d0fbb57087faa851
99hyeon <hyeon99@ajou.ac.kr>
2025-07-17 20:05:03 +0900

    [#145] refactor: ManageCookie 리팩터링

--- src/main/java/com/beour/global/jwt/ManageCookie.java
+++ src/main/java/com/beour/global/jwt/ManageCookie.java
@@ -7,9 +7,8 @@ public class ManageCookie {
     public static Cookie createCookie(String key, String value){
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(24 * 60 * 60); //refresh와 값 같게
-        //todo : 운영 배포시에 아래 주석 활성화
-        //cookie.setSecure(true);
-        //cookie.setPath("/");
+        cookie.setSecure(true);
+        cookie.setPath("/");
         cookie.setHttpOnly(true);
 
         return cookie;
